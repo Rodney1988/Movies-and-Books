069f9d43893259c9908ec3d1e273b546643a020f
Rodrigo Wahl <wahl@artech-is.com>
2023-08-22 16:51:17 +0200

    refactor imports MoviesField

--- src/templates/MoviesField/MoviesField.tsx
+++ src/templates/MoviesField/MoviesField.tsx
@@ -1,7 +1,5 @@
-import styled from '@emotion/styled';
-import { ImageProps, TMDBSearchResult } from '../../types/types';
+import { TMDBSearchResult } from '../../types/types';
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { truncate } from 'lodash';
 import {
   CardWrapper,
