234f0eb2cd6be752b957da8f8be50f788398d004
Rodrigo Wahl <wahl@artech-is.com>
2023-08-22 16:08:36 +0200

    add dotenv

--- src/api/Api.tsx
+++ src/api/Api.tsx
@@ -1,21 +1,21 @@
-import axios from "axios"
-import { BookTitle, TMDBSearchResult } from "../types/types"
+import axios from 'axios';
+import { BookTitle, TMDBSearchResult } from '../types/types';
 
 export const moviesSearch = async (searchValue: string) => {
-    const API_KEY = "486400b9fe15c6806425880827fd6c7f"; 
-    const url = `https://api.themoviedb.org/3/search/movie`;
-    const params = {
-        query: searchValue,
-        api_key: API_KEY,
-        }
-    const response = await axios.get(url, {params})
-    return response.data.results as TMDBSearchResult[]
-}
+  const API_KEY = process.env.REACT_APP_TMDB_KEY;
+  const url = `https://api.themoviedb.org/3/search/movie`;
+  const params = {
+    query: searchValue,
+    api_key: API_KEY,
+  };
+  const response = await axios.get(url, { params });
+  return response.data.results as TMDBSearchResult[];
+};
 
 export const getBooksByTitles = async (title: string) => {
-    const titleSpaceReplaced = title.replaceAll(" ", "-")
-    const response = await axios.get("http://openlibrary.org/search.json", {
-        params: { title: titleSpaceReplaced },
-    })
-    return response.data as BookTitle
-}
+  const titleSpaceReplaced = title.replaceAll(' ', '-');
+  const response = await axios.get('http://openlibrary.org/search.json', {
+    params: { title: titleSpaceReplaced },
+  });
+  return response.data as BookTitle;
+};
