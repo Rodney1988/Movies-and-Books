246e96e4e1b1260c39efb55c7a50155023a7fc06
Rodrigo Wahl <wahl@artech-is.com>
2023-08-22 12:44:34 +0200

    feat: progess with cards migration

--- src/templates/MoviesField.tsx
+++ src/templates/MoviesField.tsx
@@ -1,85 +1,156 @@
-import CircularProgress from "@mui/material/CircularProgress"
-import styled from "@emotion/styled"
-import { ImageProps, TMDBSearchResult } from "../types/types"
-import React from "react"
-import { Link } from "react-router-dom"
-import { truncate } from "lodash"
-
-/*
-This component renders multiple cards (as opposed to a table) 
-for the movies based on the 'searchedMovies' prop passed by the parent.
-*/
+import CircularProgress from '@mui/material/CircularProgress';
+import styled from '@emotion/styled';
+import { ImageProps, TMDBSearchResult } from '../types/types';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { truncate } from 'lodash';
 
 interface MoviesFieldProps {
-    searchedMovies: TMDBSearchResult[];
-    searchValue: string;
+  searchedMovies: TMDBSearchResult[];
+  searchValue: string;
 }
 
-export const MoviesField: React.FC<MoviesFieldProps> = ({searchedMovies, searchValue}) => {
-    console.log('searchedMovies::::', searchedMovies)
+const StyledFieldWrapperDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 15px 15px 0 15px;
+`;
 
-    return (
-        <StyledFieldWrapperDiv>
-            {searchedMovies.map((movie) => {
-                if (movie.title) {
-                    const id = movie.id
-                    return (
-                        <div className="example-2 card" key={id}>
-                            <div className="wrapper">
-                                <div className="header">
-                                    <StyledImageDiv src={movie.poster_path ? `https://image.tmdb.org/t/p/original/${movie.poster_path}` : ''}>
-                                        <div className="date">
-                                            <span className="release-date">
-                                               {movie.release_date}
-                                            </span>
-                                        </div>
-                                    </StyledImageDiv>
-                                </div>
-                                <div className="data">
-                                    <div className="content">
-                                        <h1 className="title">
-                                            {movie.title}
-                                        </h1>
-                                        <p className="text">
-                                            {movie.overview ? truncate(movie.overview, {length: 200}) : 'Unfortunately this movie has no description...'}
-                                        </p>
-                                        <Link 
-                                            to={`movies/${searchValue}/${id}`}
-                                            className="button"
-                                        >
-                                            See more
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                }
-                return null
-            })}
-        </StyledFieldWrapperDiv>
-    )
-}
+const CardWrapper = styled.div<ImageProps>`
+  position: relative;
+  overflow: hidden;
+  float: left;
+  width: 30%;
+  min-width: 300px;
+  margin: 10px;
+  background: ${({ src, fallBack }) => `url('${src ? src : 'fallBack'}')`};
+  height: 525px;
+`;
+
+// export const CardOverlay = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: flex-end;
+//   background: linear-gradient(
+//     0deg,
+//     rgb(34, 35, 38) 9%,
+//     rgba(33, 34, 37, 0.5) 42%,
+//     rgba(34, 35, 38, 0) 156%
+//   );
+//   height: 100%;
+//   width: 100%;
+//   h3,
+//   p {
+//     color: #ffffff;
+//     margin-left: 20px;
+//   }
+
+//   p {
+//     font-weight: bold;
+//   }
+
+//   pre {
+//     font-family: 'opensans-bold', sans-serif;
+//     font-size: 10px;
+//     color: #58a795;
+//     margin: 0;
+//     margin: 0 0 10px 20px;
+//     font-weight: bold;
+//   }
+//   :hover {
+//     opacity: 0.6;
+//   }
+// `;
+
+const OverlayDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(rgb(0 0 0 / 17%), rgba(0, 0, 0, 0.7));
+`;
+
+const Header = styled.div`
+  color: #fff;
+`;
+
+const DateSpan = styled.span`
+  font-size: large;
+  margin-right: 4px;
+  margin-left: 8px;
+  transform: translateY(5px);
+`;
+
+const Content = styled.div`
+  position: absolute;
+  height: 100%;
+  color: #fff;
+  padding: 1em;
+  border: 5px solid blue;
+`;
+
+const Title = styled.h1`
+  margin-top: 10px;
+  font-family: 'Open Sans', sans-serif;
+  font-weight: bold;
+`;
+
+export const MoviesField: React.FC<MoviesFieldProps> = ({
+  searchedMovies,
+  searchValue,
+}) => {
+  console.log('searchedMovies::::', searchedMovies);
+  const fallbackImage =
+    'https://media.istockphoto.com/id/540201480/photo/mysterious-unknown-person-in-the-hood-danger-in-darkness.jpg?s=170667a&w=0&k=20&c=SRa6I_Rb_BuTw6vo3OGG6PoSuKJtmaO-CViDjWj-Qkk=';
+  return (
+    <StyledFieldWrapperDiv>
+      {searchedMovies.map((movie) => {
+        if (movie.title) {
+          const id = movie.id;
+          return (
+            <CardWrapper
+              key={id}
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/original/${movie.poster_path}`
+                  : ''
+              }
+              fallBack={fallbackImage}
+            >
+              <OverlayDiv>
+                <Content>
+                  <Header>
+                    <DateSpan className="release-date">
+                      {movie.release_date}
+                    </DateSpan>
+                  </Header>
+                  <Title className="title">
+                    {movie.title
+                      ? truncate(movie.title, { length: 60 })
+                      : 'This movie has no title...'}
+                  </Title>
+                  <p className="text">
+                    {movie.overview
+                      ? truncate(movie.overview, { length: 200 })
+                      : 'Unfortunately this movie has no description...'}
+                  </p>
+                  <Link to={`movies/${searchValue}/${id}`} className="button">
+                    See more
+                  </Link>
+                </Content>
+              </OverlayDiv>
+            </CardWrapper>
+          );
+        }
+        return null;
+      })}
+    </StyledFieldWrapperDiv>
+  );
+};
 
 export const StyledCircularProgress = styled(CircularProgress)`
-    margin-top: 100px;
-`
-
-export const StyledFieldWrapperDiv = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    margin: 15px 15px 0 15px;
-`
-
-const StyledImageDiv = styled.div<ImageProps>`
-    background: linear-gradient(rgb(0 0 0 / 17%), rgba(0, 0, 0, 0.7)),
-        url(${(props) =>
-                !!props.src
-                    ? props.src
-                    : "https://media.istockphoto.com/id/540201480/photo/mysterious-unknown-person-in-the-hood-danger-in-darkness.jpg?s=170667a&w=0&k=20&c=SRa6I_Rb_BuTw6vo3OGG6PoSuKJtmaO-CViDjWj-Qkk="})
-            center/cover no-repeat;
-    height: 525px;
-`
-
+  margin-top: 100px;
+`;
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,118 +1,119 @@
 export interface UserRating {
-    numeric_description_only: number
-    description: string
-    rating: string
-    numeric_rating_only: number
+  numeric_description_only: number;
+  description: string;
+  rating: string;
+  numeric_rating_only: number;
 }
 
 export interface ReleaseDate {
-    URL: string
-    NAME: string
+  URL: string;
+  NAME: string;
 }
 
 export interface Summary {
-    "Full Cast": string
-    plot: string
+  'Full Cast': string;
+  plot: string;
 }
 export interface TMDBSearchResult {
-    adult: boolean
-    backdrop_path?: string
-    genre_ids: number[]
-    id: number
-    original_language: string
-    original_title: string
-    overview: string
-    popularity: number
-    poster_path?: string
-    release_date: string
-    title: string
-    video: boolean
-    vote_average: number
-    vote_count: number
-  }  
-
-export type ImageProps = {
-    src?: string
+  adult: boolean;
+  backdrop_path?: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path?: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
 
+export type ImageProps = {
+  src?: string;
+  fallBack: string;
+};
+
 export interface BookTitle {
-    numFound: number
-    start: number
-    numFoundExact: boolean
-    docs: Doc[]
-    num_found: number
-    q: string
-    offset?: any
+  numFound: number;
+  start: number;
+  numFoundExact: boolean;
+  docs: Doc[];
+  num_found: number;
+  q: string;
+  offset?: any;
 }
 
 export interface Doc {
-    key: string
-    type: string
-    seed: string[]
-    title: string
-    title_suggest: string
-    edition_count: number
-    edition_key: string[]
-    publish_date: string[]
-    publish_year: number[]
-    first_publish_year: number
-    number_of_pages_median: number
-    lccn: string[]
-    publish_place: string[]
-    oclc: string[]
-    contributor: string[]
-    lcc: string[]
-    ddc: string[]
-    isbn: string[]
-    last_modified_i: number
-    ebook_count_i: number
-    ebook_access: string
-    has_fulltext: boolean
-    public_scan_b: boolean
-    ia: string[]
-    ia_collection: string[]
-    ia_collection_s: string
-    lending_edition_s: string
-    lending_identifier_s: string
-    printdisabled_s: string
-    cover_edition_key: string
-    cover_i: number
-    publisher: string[]
-    language: string[]
-    author_key: string[]
-    author_name: string[]
-    author_alternative_name: string[]
-    person: string[]
-    place: string[]
-    subject: string[]
-    time: string[]
-    id_alibris_id: string[]
-    id_amazon: string[]
-    id_better_world_books: string[]
-    id_canadian_national_library_archive: string[]
-    id_depósito_legal: string[]
-    id_goodreads: string[]
-    id_google: string[]
-    id_librarything: string[]
-    id_overdrive: string[]
-    id_paperback_swap: string[]
-    id_wikidata: string[]
-    ia_loaded_id: string[]
-    ia_box_id: string[]
-    publisher_facet: string[]
-    person_key: string[]
-    place_key: string[]
-    time_facet: string[]
-    person_facet: string[]
-    subject_facet: string[]
-    _version_: any
-    place_facet: string[]
-    lcc_sort: string
-    author_facet: string[]
-    subject_key: string[]
-    ddc_sort: string
-    time_key: string[]
-    first_sentence: string[]
-    subtitle: string
-    id_bcid: string[]
+  key: string;
+  type: string;
+  seed: string[];
+  title: string;
+  title_suggest: string;
+  edition_count: number;
+  edition_key: string[];
+  publish_date: string[];
+  publish_year: number[];
+  first_publish_year: number;
+  number_of_pages_median: number;
+  lccn: string[];
+  publish_place: string[];
+  oclc: string[];
+  contributor: string[];
+  lcc: string[];
+  ddc: string[];
+  isbn: string[];
+  last_modified_i: number;
+  ebook_count_i: number;
+  ebook_access: string;
+  has_fulltext: boolean;
+  public_scan_b: boolean;
+  ia: string[];
+  ia_collection: string[];
+  ia_collection_s: string;
+  lending_edition_s: string;
+  lending_identifier_s: string;
+  printdisabled_s: string;
+  cover_edition_key: string;
+  cover_i: number;
+  publisher: string[];
+  language: string[];
+  author_key: string[];
+  author_name: string[];
+  author_alternative_name: string[];
+  person: string[];
+  place: string[];
+  subject: string[];
+  time: string[];
+  id_alibris_id: string[];
+  id_amazon: string[];
+  id_better_world_books: string[];
+  id_canadian_national_library_archive: string[];
+  id_depósito_legal: string[];
+  id_goodreads: string[];
+  id_google: string[];
+  id_librarything: string[];
+  id_overdrive: string[];
+  id_paperback_swap: string[];
+  id_wikidata: string[];
+  ia_loaded_id: string[];
+  ia_box_id: string[];
+  publisher_facet: string[];
+  person_key: string[];
+  place_key: string[];
+  time_facet: string[];
+  person_facet: string[];
+  subject_facet: string[];
+  _version_: any;
+  place_facet: string[];
+  lcc_sort: string;
+  author_facet: string[];
+  subject_key: string[];
+  ddc_sort: string;
+  time_key: string[];
+  first_sentence: string[];
+  subtitle: string;
+  id_bcid: string[];
 }
