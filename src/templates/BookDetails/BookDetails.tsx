b2de686946c86df1bad3466bb5dad6e145188a3e
Rodrigo Wahl <wahl@artech-is.com>
2023-08-25 11:37:21 +0200

    fix: add Bookdetail fixes

--- src/templates/BookDetails/BookDetails.tsx
+++ src/templates/BookDetails/BookDetails.tsx
@@ -6,76 +6,77 @@ import { capitalize } from 'lodash';
 export const BookDetails = ({ rowState }: { rowState: Doc }) => {
   console.log(rowState, 'ROWSTAAAAATE');
   return (
-    <>
-      <StyledDetailTitle variant="h5">
-        Details about the book '{rowState?.title}'
-      </StyledDetailTitle>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
       <StyledPaperDetails square>
         <div style={{ padding: '10px' }}>
-          {rowState?.title && (
+          {rowState.title && (
             <div style={{ margin: '2px' }}>
-              {<b>{'Title'}</b>} - {rowState?.title}
+              {<b>{'Title'}</b>} - {rowState.title}
             </div>
           )}
-          {rowState?.author_name && (
+          {rowState.author_name && (
             <div style={{ margin: '2px' }}>
-              {<b>{'Author(s)'}</b>} - {rowState?.author_name.join(', ')}
+              {<b>{'Author(s)'}</b>} - {rowState.author_name.join(', ')}
             </div>
           )}
-          {rowState?.contributor && (
+          {rowState.contributor && (
             <div style={{ margin: '2px' }}>
-              {<b>{'Contributors'}</b>} - {rowState?.contributor.join(' ')}
+              {<b>{'Contributors'}</b>} - {rowState.contributor.join(' ')}
             </div>
           )}
-          {rowState?.first_publish_year && (
+          {rowState.first_publish_year && (
             <div style={{ margin: '2px' }}>
-              {<b>{'First Published'}</b>} - {rowState?.first_publish_year}
+              {<b>{'First Published'}</b>} - {rowState.first_publish_year}
             </div>
           )}
-          {rowState?.first_sentence && (
+          {rowState.first_sentence && (
             <div style={{ margin: '2px' }}>
-              {<b>{'First Sentences'}</b>} -{' '}
-              {rowState?.first_sentence.join(' ')}
+              {<b>{'First Sentences'}</b>} - {rowState.first_sentence.join(' ')}
             </div>
           )}
-          {rowState?.ebook_access && (
+          {rowState.ebook_access && (
             <div style={{ margin: '2px' }}>
-              {<b>{'E-Book Access'}</b>} - {capitalize(rowState?.ebook_access)}
+              {<b>{'E-Book Access'}</b>} - {capitalize(rowState.ebook_access)}
             </div>
           )}
-          {rowState?.ebook_count_i && (
+          {rowState.ebook_count_i > 0 && (
             <div style={{ margin: '2px' }}>
-              {<b>{'E-Book Count'}</b>} - {rowState?.ebook_count_i}
+              {<b>{'E-Book Count'}</b>} - {rowState.ebook_count_i}
             </div>
           )}
-          {rowState?.edition_count && (
+          {rowState.edition_count > 0 && (
             <div style={{ margin: '2px' }}>
-              {<b>{'Edition Count'}</b>} - {rowState?.edition_count}
+              {<b>{'Edition Count'}</b>} - {rowState.edition_count}
             </div>
           )}
-          {rowState?.language && (
+          {rowState.language && (
             <div style={{ margin: '2px' }}>
-              {<b>{'Language(s)'}</b>} - {rowState?.language.join(', ')}
+              {<b>{'Language(s)'}</b>} - {rowState.language.join(', ')}
             </div>
           )}
-          {rowState?.subject && (
+          {rowState.subject && (
             <div style={{ margin: '2px' }}>
-              {<b>{'Subject'}</b>} - {rowState?.subject.join(', ')}
+              {<b>{'Subject'}</b>} - {rowState.subject.join(', ')}
             </div>
           )}
-          {rowState?.id_amazon && (
+          {rowState.id_amazon && (
             <div style={{ margin: '2px' }}>
-              {<b>{'Amazon IDs'}</b>} - {rowState?.id_amazon.join(', ')}
+              {<b>{'Amazon IDs'}</b>} - {rowState.id_amazon.join(', ')}
             </div>
           )}
-          {rowState?.key && (
+          {rowState.key && (
             <div style={{ margin: '2px' }}>
-              {<b>{'Key'}</b>} - {rowState?.key}
+              {<b>{'Key'}</b>} - {rowState.key}
             </div>
           )}
         </div>
       </StyledPaperDetails>
-    </>
+    </div>
   );
 };
 
