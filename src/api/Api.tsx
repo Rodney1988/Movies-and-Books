9ad4c8335ded707c2fff2ff17d42f661d1d231e4
Rodrigo Wahl <wahl@artech-is.com>
2023-08-30 17:19:05 +0200

    fix: https

--- src/api/Api.tsx
+++ src/api/Api.tsx
@@ -25,7 +25,7 @@ export const singleMovieSearch = async (movieId: number) => {
 
 export const getBooksByTitles = async (title: string) => {
   const titleSpaceReplaced = title.replaceAll(' ', '-');
-  const response = await axios.get('http://openlibrary.org/search.json', {
+  const response = await axios.get('https://openlibrary.org/search.json', {
     params: { title: titleSpaceReplaced },
   });
   return response.data as ByBookTitle;
